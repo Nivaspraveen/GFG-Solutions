695ee823ed2def203a2d1e786ab00c5cb954d658
nivas_pravin75 <51258356+Nivaspraveen@users.noreply.github.com>
2024-11-07 16:16:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.js
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+function printlist(head) {
+    if (!head) return;
+    let current = head;
+    let s = '';
+    while (current !== null) {
+        s += current.data + " ";
+        current = current.next;
+    }
+    console.log(s);
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+
+        let input_ar1 = readLine().split(' ').map(x => parseInt(x));
+        let head = new Node(input_ar1[0]);
+        let tail = head;
+        for (let i = 1; i < input_ar1.length; i++) {
+            tail.next = new Node(input_ar1[i]);
+            tail = tail.next;
+        }
+        let obj = new Solution();
+        let res = obj.removeDuplicates(head);
+        printlist(res);
+
+        console.log("~");
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/**
+ * @param {Node} head
+ * @returns {Node}
+ */
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    // Function to remove duplicates from sorted linked list.
+    removeDuplicates(head) {
+        let current = head;
+        while (current && current.next) {
+            current.next = (current.data === current.next.data) ? current.next.next : current.next;
+            if (current.data !== current.next?.data) current = current.next
+        }
+        return head;
+    }
+}
