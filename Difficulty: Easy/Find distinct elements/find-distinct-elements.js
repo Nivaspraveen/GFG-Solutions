37c56009522559a38e5d12650c11dd247a001fc7
nivas_pravin75 <51258356+Nivaspraveen@users.noreply.github.com>
2024-11-07 17:00:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find distinct elements/find-distinct-elements.js
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let arr = readLine().split(' ').map(x => parseInt(x, 10));
+        console.log(distinct(arr));
+
+        console.log("~");
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for javascript
+function distinct(arr) {
+    const distEl = new Set(arr);
+    return distEl.size;
+}
